d66ee059c855415c83f527702d23df8520a45342
Sanjay Kumar <ersanjay426@gmail.com>
2026-06-18 20:10:10 +0530

    Initial commit

---
+++ src/main/java/com/homekart/file_processing_service/FileProcessingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.homekart.file_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessingServiceApplication.class, args);
+		System.out.println("File processing service running... ");
+	}
+
+}
